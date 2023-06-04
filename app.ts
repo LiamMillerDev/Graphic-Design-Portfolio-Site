98eb2b7bf0fb3813f50892b993786bb004d9c582
Liam Miller <LiamJmillerNZ@gmail.com>
2023-06-04 23:22:24 +1200

    Added portfolio layout

---
+++ app.ts
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuImage = document.getElementById("menuImage") as HTMLImageElement; // Cast to HTMLImageElement
+  const menuOptions = document.getElementById("menuOptions");
+
+  menuImage?.addEventListener("click", () => {
+    if (menuOptions) {
+      menuOptions.classList.toggle("show");
+      // Toggle the image source based on whether the menu is shown or not
+      if (menuOptions.classList.contains("show")) {
+        // When the menu is open, show the close icon
+        if (menuImage) menuImage.src = "/assets/close-menu.png";
+      } else {
+        // When the menu is closed, show the hamburger icon
+        if (menuImage) menuImage.src = "/assets/burger-menu.png";
+      }
+    }
+  });
+});
