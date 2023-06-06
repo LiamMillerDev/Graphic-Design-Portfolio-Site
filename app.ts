41c5ee022937b47edd2ba8417bb2bae1785f6097
Liam Miller <LiamJmillerNZ@gmail.com>
2023-06-07 10:25:12 +1200

    Added about content

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Swiper from "swiper";
+import Swiper, { Navigation, Pagination } from "swiper";
 
+Swiper.use([Navigation, Pagination]);
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -21,4 +22,48 @@ document.addEventListener("DOMContentLoaded", function () {
       overlay.classList.toggle("slide");
     });
   }
+
+  // Initialize Swiper
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      640: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  // Animate cards on hover
+  document.querySelectorAll(".about-card").forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+      gsap.to(card, {
+        scale: 1.05,
+        duration: 0.3,
+        ease: "power2.inOut",
+      });
+    });
+
+    card.addEventListener("mouseleave", () => {
+      gsap.to(card, {
+        scale: 1,
+        duration: 0.3,
+        ease: "power2.inOut",
+      });
+    });
+  });
 });
