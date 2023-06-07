965f6438008598387724c40b06dec9631b5164fa
Liam Miller <LiamJmillerNZ@gmail.com>
2023-06-08 10:19:38 +1200

    Upaded swiper

--- app.ts
+++ app.ts
@@ -23,51 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
       overlay.classList.toggle("slide");
     });
   }
-
-  // Initialize Swiper
   const swiper = new Swiper(".swiper-container", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true, // Adding loop option
+    loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
     },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      640: {
-        slidesPerView: 3,
-      },
-    },
-  }) as Swiper;
-
-  // Animate cards on hover
-  document.querySelectorAll(".about-card").forEach((card: Element) => {
-    if (card instanceof HTMLElement) {
-      card.addEventListener("mouseenter", () => {
-        gsap.to(card, {
-          scale: 1.05,
-          duration: 0.3,
-          ease: "power2.inOut",
-        });
-      });
-
-      card.addEventListener("mouseleave", () => {
-        gsap.to(card, {
-          scale: 1,
-          duration: 0.3,
-          ease: "power2.inOut",
-        });
-      });
-    }
   });
 });
--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const gsap_1 = require("gsap");
 const ScrollTrigger_1 = require("gsap/ScrollTrigger");
+const swiper_1 = __importStar(require("swiper"));
+require("swiper/swiper-bundle.css");
+swiper_1.default.use([swiper_1.Navigation, swiper_1.Pagination]);
 gsap_1.gsap.registerPlugin(ScrollTrigger_1.ScrollTrigger);
 document.addEventListener("DOMContentLoaded", function () {
     const icon1 = document.getElementById("icon1");
@@ -19,4 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
             overlay.classList.toggle("slide");
         });
     }
+    const swiper = new swiper_1.default(".swiper-container", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
 });
