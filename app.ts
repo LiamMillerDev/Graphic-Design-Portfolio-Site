104990636eb4bcdda322ea44336ea7141ac86e04
Liam Miller <LiamJmillerNZ@gmail.com>
2023-06-07 17:08:33 +1200

    Changed swiper slide

--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 
 Swiper.use([Navigation, Pagination]);
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const icon1 = document.getElementById("icon1");
-  const icon2 = document.getElementById("icon2");
-  const icon3 = document.getElementById("icon3");
-  const menuIcon = document.getElementById("menuIcon");
-  const menuOptions = document.getElementById("menuOptions");
-  const overlay = document.getElementById("overlay");
+  const icon1 = document.getElementById("icon1") as HTMLElement;
+  const icon2 = document.getElementById("icon2") as HTMLElement;
+  const icon3 = document.getElementById("icon3") as HTMLElement;
+  const menuIcon = document.getElementById("menuIcon") as HTMLElement;
+  const menuOptions = document.getElementById("menuOptions") as HTMLElement;
+  const overlay = document.getElementById("overlay") as HTMLElement;
 
   if (icon1 && icon2 && icon3 && menuIcon && menuOptions && overlay) {
     menuIcon.addEventListener("click", () => {
@@ -27,6 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper-container", {
     slidesPerView: 3,
     spaceBetween: 10,
+    loop: true, // Adding loop option
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -46,24 +48,26 @@ document.addEventListener("DOMContentLoaded", function () {
         slidesPerView: 3,
       },
     },
-  });
+  }) as Swiper;
 
   // Animate cards on hover
-  document.querySelectorAll(".about-card").forEach((card) => {
-    card.addEventListener("mouseenter", () => {
-      gsap.to(card, {
-        scale: 1.05,
-        duration: 0.3,
-        ease: "power2.inOut",
+  document.querySelectorAll(".about-card").forEach((card: Element) => {
+    if (card instanceof HTMLElement) {
+      card.addEventListener("mouseenter", () => {
+        gsap.to(card, {
+          scale: 1.05,
+          duration: 0.3,
+          ease: "power2.inOut",
+        });
       });
-    });
 
-    card.addEventListener("mouseleave", () => {
-      gsap.to(card, {
-        scale: 1,
-        duration: 0.3,
-        ease: "power2.inOut",
+      card.addEventListener("mouseleave", () => {
+        gsap.to(card, {
+          scale: 1,
+          duration: 0.3,
+          ease: "power2.inOut",
+        });
       });
-    });
+    }
   });
 });
